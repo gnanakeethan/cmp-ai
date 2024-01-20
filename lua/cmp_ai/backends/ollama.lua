a0cd6b1706a9d2c883785c48ffe79749c2e6a2d5
Gnanakeethan Balasubramaniam <gnanakeethan@users.noreply.github.com>
2024-01-20 06:03:24 +0530

    Update ollama.lua

--- lua/cmp_ai/backends/ollama.lua
+++ lua/cmp_ai/backends/ollama.lua
@@ -8,7 +8,7 @@ function Ollama:new(o, params)
   self.__index = self
   self.params = vim.tbl_deep_extend('keep', params or {}, {
     base_url = 'http://127.0.0.1:11434/api/generate',
-    model = 'codellama:34b-code',
+    model = 'codellama:7b-code-fp16',
     options = {
       temperature = 0.2,
     },
